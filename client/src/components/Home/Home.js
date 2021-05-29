556360735f2136036cb03be9d0cdd2858f55e081
nefzi chayma <nefzi.chayma@gmail.com>
2021-05-29 22:20:09 +0200

    first commit

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="col-md-6 offset-md-3 text-center">
+      <h3 className="pt-4">welcom in My Library</h3>
+      <hr />
+      <p className="lead">
+        Please sign up
+      </p>
+      <footer>Created by &copy; nefzi chayma  2021</footer>
+    </div>
+  );
+};
+export default Home;
